c199ab6b419584adf1e94495c4985cc159777d03
Abhisekh Mishra <abhisekhmishra26@gmail.com>
2025-06-08 11:51:01 +0530

    cloudinary image storage for user profile

--- src/main/java/com/example/KrishiConnect/config/CloudinaryConfig.java
+++ src/main/java/com/example/KrishiConnect/config/CloudinaryConfig.java
@@ -2,15 +2,18 @@ package com.example.KrishiConnect.config;
 
 import com.cloudinary.Cloudinary;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.url}")
+    String cloudinaryUrl;
+
     @Bean
-    Cloudinary cloudinary(){
-        Dotenv dotenv = Dotenv.load();
-        return new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+    Cloudinary cloudinary() {
+        return new Cloudinary(cloudinaryUrl);
     }
 }
